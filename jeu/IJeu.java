00dc811ba1fc6f54e14f2221e4e3323a2a7e0047
Thibaut Quentin <thibaut.quentin@laposte.net>
2014-12-09 11:57:05 +0000

    Added IJeu interface
    Signed-off-by: Thibaut Quentin <thibaut.quentin@laposte.net>

---
+++ jeu/IJeu.java
@@ -0,0 +1,8 @@
+package jeu;
+
+public interface IJeu {
+    public String getEtatInitial();
+    public String getOrdreDuTour();
+    public String action ( String action);
+    public String Classement ();
+}
