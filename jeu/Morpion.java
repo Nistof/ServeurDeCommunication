55e9e0daac7f9a6c8165faddbef1a68b76631d7c
Martin Florian <nistof.contact@gmail.com>
2014-12-14 19:38:05 +0100

    Morpion

---
+++ jeu/Morpion.java
@@ -0,0 +1,75 @@
+public class Morpion {
+	private int player;
+	private char[][] grid;
+	
+	public Morpion() {
+		this.player = 0;
+		this.grid = new char[3][3];
+		
+		for ( int j = 0; j < grid.length; j++)
+			for ( int i = 0; i < grid[0].length; i++)
+				grid[i][j] = ' ';		
+	}
+	
+	public boolean action (String action) {
+		if ( canPlay() ) {
+			String[] pos = action.split(" ");
+			int x, y;
+			if ( pos.length != 2)
+				return false;
+			
+			x = Integer.parseInt(pos[0]);
+			y = Integer.parseInt(pos[1]);
+			
+			if ( grid[x][y] == 'X' || grid[x][y] == 'O')
+				return false;
+			
+			grid[x][y] = (player == 0)?'X':'O';
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean canPlay() {
+		boolean b = false;
+		
+		for ( int j = 0; j < grid.length; j++)
+			for ( int i = 0; i < grid[0].length; i++)
+				if ( grid[i][j] == ' ' )
+					b = true;
+		return b;
+	}
+	
+	public boolean win() {
+		return 	( grid[1][1] == 'X' || grid[1][1] == 'O') &&
+				(( grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1] ) ||
+				( grid[1][0] == grid[1][1] && grid[1][0] == grid[1][2] ) ||
+				( grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2] ) ||
+				( grid[0][2] == grid[1][1] && grid[0][2] == grid[2][0] ))||
+				( grid[0][0] == 'X' || grid[0][0] == 'O') &&
+				(( grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0] ) ||
+				( grid[0][0] == grid[0][1] && grid[0][0] == grid[0][2] ))||
+				( grid[2][2] == 'X' || grid[2][2] == 'O') &&
+				(( grid[0][2] == grid[1][2] && grid[0][2] == grid[2][2] ) ||
+				( grid[2][0] == grid[2][1] && grid[2][0] == grid[2][2] ));
+	}
+	
+	public void changePlayer () { player = (player+1)%2; }
+	public char getPlayer () { return (player == 0)?'X':'O'; }
+	
+	public String toString() {
+		String str = "";
+		String sep = "______\n";
+		
+		str += sep;
+		for ( int j = 0; j < grid.length; j++) {
+			str += "|";
+			for ( int i = 0; i < grid[0].length; i++)
+				str += grid[i][j] + "|";
+			str += "\n" + sep;
+		}
+		
+		return str;
+	}
+}
