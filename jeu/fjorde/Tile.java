0b1d4541b77fe89fbaa964466460bb745dd9ab75
Thibaut Quentin <thibaut.quentin@laposte.net>
2015-01-05 15:02:05 +0000

    Added methods to set neighoors and types of a Tile
    Signed-off-by: Thibaut Quentin <thibaut.quentin@laposte.net>

--- jeu/fjorde/Tile.java
+++ jeu/fjorde/Tile.java
@@ -1,8 +1,12 @@
 package jeu.fjorde;
 
+import jeu.Player;
+
 public class Tile {
     
     private Type[] types;
+    private Tile[] neighboors;
+    private Player owner;
     private boolean start;
     private Item item;
     
@@ -13,25 +17,49 @@ public class Tile {
      */
     public Tile (boolean start) {
         this.types = new Type[6];
+        this.neighboors = new Tile[6];
         this.start = start;
         this.item = null;
     }
-    
+
     /**
-     * 
-     * @param item
-     * @return
+     * Permet de poser un item sur une tuile
+     * @param item L'item à poser
+     * @param p Le joueur détenant l'item
+     * @return vrai si la pose de l'item a réussi
      */
-    public boolean setItem (Item item) {
-        boolean b = true;
-        if(this.item != null || item == null) {
-            b = false;
-        }
-        else {
+    public boolean setItem (Item item, Player p) {
+        boolean b = false;
+        if(this.item == null && item != null) {
             this.item = item;
+            owner = p;
+            b = true;
         }
         return b;
     }
     
+    /**
+     * Place une tuile voisine 
+     * @param id Coté à utiliser
+     * @param t Tuile à placer
+     * @return vrai si la pose de la tuile a réussi
+     */
+    public boolean setNeighboorById (int id, Tile t) {
+        boolean b = false;
+        if(neighboors[id] == null) {
+            neighboors[id] = t;
+            b = true;
+        }
+        return b;
+    }
     
+    /**
+     * Permet de fixer les 6 types de la tuile
+     * @param types la chaîne à utiliser pour définir les types
+     */
+    public void setTypes (String types) {
+        for(int i = 0; i < types.length() ; i++) {
+            this.types[i] = Type.getTypeById(types.charAt(i));
+        }
+    }
 }
--- jeu/fjorde/Type.java
+++ jeu/fjorde/Type.java
@@ -4,4 +4,17 @@ public enum Type {
     TERRE_ARABLE,
     MONTAGNE,
     EAU;
+
+    public static Type getTypeById (char id) {
+        switch(id) {
+            case 'T':
+                return Type.TERRE_ARABLE;
+            case 'M':
+                return Type.MONTAGNE;
+            case 'E':
+                return Type.EAU;
+            default:
+                return null;
+        }
+    }
 }
