a77ce3fdb36f894eb6ff0813c940678f912a7f45
Martin Florian <nistof.contact@gmail.com>
2015-01-11 14:04:49 +0100

    What have you done Eclipse? :o

--- client/Fjorde/GridFjorde.java
+++ client/Fjorde/GridFjorde.java
@@ -1,42 +1,3 @@
-<<<<<<< Upstream, based on origin/master
-package client.Fjorde;
-
-import java.util.ArrayList;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class GridFjorde extends JPanel {
-	ArrayList<Tile> tiles;
-	
-	public GridFjorde() {
-		this.setLayout(null);
-		this.tiles = new ArrayList<Tile>();
-	}
-	
-	public void createTest() {
-		this.tiles.add(new Tile("MMMMMM", 0));
-		this.tiles.add(new Tile("ETMMTE", 2));
-		this.tiles.add(new Tile("EEEETE", 5));
-		
-		tiles.get(0).setLocation(this.getWidth()/2, this.getHeight()/2);
-		tiles.get(1).setLocationWithTile(tiles.get(0), 0);
-		tiles.get(2).setLocationWithTile(tiles.get(1), 2);
-		
-		for (Tile t : tiles) {
-			JLabel tile = t.getGraphicTile();
-			tile.setBounds(t.getX(), t.getY(), Tile.IMG_WIDTH, Tile.IMG_HEIGHT);
-			System.out.println(t.getX() + ":" + t.getY());
-			this.add(tile);
-		}
-	}
-	
-	public void addTile(Tile tile) {
-		if (tile != null)
-			this.tiles.add(tile);
-	}
-}
-=======
 package client.Fjorde;
 
 import java.util.ArrayList;
@@ -102,4 +63,3 @@ public class GridFjorde extends JPanel {
 			this.tiles.add(tile);
 	}
 }
->>>>>>> abb039d IHM for Fjorde continued
